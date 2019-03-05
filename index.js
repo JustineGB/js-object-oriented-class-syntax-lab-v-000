a293189fd6de8d27b30ef5d26e954646397923b0
Justine B <justinegayton@gmail.com>
2019-03-05 20:43:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,9 +28,11 @@ class BoardMember {
   }
 }
 
-class BoardMember {
+class Ceo extends BoardMember {
   constructor(name, homeState, training) {
     this.name = name;
     this.homeState = homeState;
     this.training = training;
   }
+  hireEmployee(){}
+}
