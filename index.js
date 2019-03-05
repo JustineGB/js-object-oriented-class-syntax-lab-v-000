a56fb86007fccef1e0cbd48e6ac7f9cb7acdac07
Justine B <justinegayton@gmail.com>
2019-03-05 20:42:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,3 +27,10 @@ class BoardMember {
     
   }
 }
+
+class BoardMember {
+  constructor(name, homeState, training) {
+    this.name = name;
+    this.homeState = homeState;
+    this.training = training;
+  }
